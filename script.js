f6513dd49db5fd1a50b6563f1707bf48a74fea0b
jvdlopes <jvdlopes04@gmail.com>
2026-02-13 14:35:33 -0500

    finished edit button

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ function makeBooking(){
     phoneNumber.innerText = "Phone Number: " + phoneNumberForm.value;
     barberForm = document.getElementById("barber-form");
     barber = document.getElementById("barber");
-    barber.innerText = "Barber: " + barberForm.options[barberForm.selectedIndex].text;
+    barber.innerText = "Barber: " + barberForm.value;
     haircutForm = document.getElementById("haircut-form");
     haircut = document.getElementById("haircut");
-    haircut.innerText = "Haircut: " + haircutForm.options[haircutForm.selectedIndex].text;
+    haircut.innerText = "Haircut: " + haircutForm.value;
     dateForm = document.getElementById("date-form");
     date = document.getElementById("date");
     date.innerText = "Date: " + dateForm.value;
@@ -40,6 +40,7 @@ function makeBooking(){
     form.reset();
     alert("Booking Successful!");
     document.getElementById("clearBttn").hidden = false;
+    document.getElementById("editBttn").hidden = false;
 }
 
 function clearBooking(){
@@ -66,4 +67,30 @@ function clearBooking(){
     notes.hidden = true;
 
     document.getElementById("clearBttn").hidden = true;
+    document.getElementById("editBttn").hidden = true;
+}
+
+function editBooking(){
+    nameForm = document.getElementById("name-form");
+    nameBooking = document.getElementById("name");
+    nameForm.value = nameBooking.innerText.split(": ")[1];
+    phoneNumberForm = document.getElementById("phone-number-form");
+    phoneNumber = document.getElementById("phone-number");
+    phoneNumberForm.value = phoneNumber.innerText.split(": ")[1];
+    barberForm = document.getElementById("barber-form");
+    barber = document.getElementById("barber");
+    barberForm.value = barber.innerText.split(": ")[1];
+    haircutForm = document.getElementById("haircut-form");
+    haircut = document.getElementById("haircut");
+    haircutForm.value = haircut.innerText.split(": ")[1];
+    dateForm = document.getElementById("date-form");
+    date = document.getElementById("date");
+    dateForm.value = date.innerText.split(": ")[1];
+    timeForm = document.getElementById("time-form");
+    time = document.getElementById("time"); 
+    timeForm.value = time.innerText.split(": ")[1];
+    notesForm = document.getElementById("notes-form");
+    notes = document.getElementById("notes"); 
+    notesForm.value = notes.innerText.split(": ")[1];
+    clearBooking();
 }
